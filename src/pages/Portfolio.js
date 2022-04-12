839c1fe466e925147ae152427de25d73220fb20d
Oscar Guzman <oscarprograb@gmail.com>
2022-04-11 23:49:34 -0500

    Portafolio

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,7 +1,8 @@
-import {CardContent, Chip, Paper} from "@mui/material";
+import {Chip, Link, Paper} from "@mui/material";
 import Typography from "@mui/material/Typography";
 import {useTranslation} from "react-i18next";
 import DataTable from "../components/DataTable";
+import {Language} from "@mui/icons-material";
 
 const DrawTechnologies = (technologies) => {
     const getRandomColor = () => {
@@ -35,6 +36,20 @@ const DrawTechnologies = (technologies) => {
         )
     })}</div>);
 }
+const DrawLinks = (links) => {
+    return (<div>{links.map((link) => {
+            return (<Chip
+                key={`chip-${link.name}`}
+                size="small"
+                label={<Link
+                    key={`link-${link.name}`}
+                    target="_blank"
+                    href={link.link}>{link.name}</Link>}
+                icon={<Language/>}/>)
+        }
+    )
+    }</div>);
+}
 
 const Portfolio = () => {
     const {t: translation} = useTranslation();
@@ -46,26 +61,79 @@ const Portfolio = () => {
     }, {
         id: 'date', title: translation("portfolio_table_date"), dataSource: 'date',
     }, {
+        id: 'links', title: translation("portfolio_table_links"), dataSource: 'link',
+        drawFunction: (rowElement) => DrawLinks(rowElement.links)
+    }, {
         id: 'description', title: translation("portfolio_table_description"), dataSource: 'description',
     }];
     const portfolioData = [
         {
-            projectName: '',
-            date: '',
-            description: '',
-            technologies: ['java','python']
-        }
+            projectName: 'OscarGPDev',
+            date: '04/2022',
+            description: translation("portfolio_table_oscargpdev_description"),
+            technologies: ['React', 'JavaScript'],
+            links: []
+        },
+        {
+            projectName: 'DeliDeliCake (Vue)',
+            date: '03/2022',
+            description: translation("portfolio_table_delidelicake_vue_description"),
+            technologies: ['HTML', 'CSS', 'JavaScript', 'Vue.js'],
+            links: [{name: translation('portfolio_table_link_to_site'), link: 'https://delidelicakevue.web.app/'},
+                {
+                    name: 'GitHub',
+                    link: 'https://github.com/OscarGPDev/LaunchX_exercises/tree/main/FrontendMision/Semana%205%20-%20Comparando%20naves'
+                }]
+        },
+        {
+            projectName: 'Pokédex',
+            date: '03/2022',
+            description: translation("portfolio_table_pokedex_description"),
+            technologies: ['HTML', 'CSS', 'JavaScript'],
+            links: [{name: translation('portfolio_table_link_to_site'), link: 'https://pokedex-83b18.web.app/'},
+                {
+                    name: 'GitHub',
+                    link: 'https://github.com/OscarGPDev/LaunchX_exercises/tree/main/FrontendMision/Semana%204%20-%20Controlando%20el%20vuelo%20con%20JS'
+                }]
+        },
+        {
+            projectName: 'Vaccines',
+            date: '03/2022',
+            description: translation("portfolio_table_vaccines_description"),
+            technologies: ['HTML', 'CSS'],
+            links: [{name: translation('portfolio_table_link_to_site'), link: 'https://vaccines-82d85.web.app/'},
+                {
+                    name: 'GitHub',
+                    link: 'https://github.com/OscarGPDev/LaunchX_exercises/tree/main/FrontendMision/Semana%203%20-%20Volando%20con%20estilo'
+                }]
+        },
+        {
+            projectName: 'DeliDeliCake',
+            date: '03/2022',
+            description: translation("portfolio_table_delidelicake_description"),
+            technologies: ['HTML', 'CSS', 'JavaScript', 'Bootstrap'],
+            links: [{name: translation('portfolio_table_link_to_site'), link: 'https://delidelicake-3ff7e.web.app/'},
+                {
+                    name: 'GitHub',
+                    link: 'https://github.com/OscarGPDev/LaunchX_exercises/tree/main/FrontendMision/Semana%202%20-%20Primer%20vuelo'
+                }]
+        },
+        {
+            projectName: 'MuseumMax',
+            date: '01/2022',
+            description: translation("portfolio_table_museummax_description"),
+            technologies: ['React', 'MUI', 'Django-Rest-Framework', 'MariaDB', 'Firebase', 'Python'],
+            links: [{name: translation('portfolio_table_link_to_site'), link: 'https://museummax-20c21.web.app/'}]
+        },
     ];
     return (<Paper elevation={0}>
-        <CardContent sx={{padding: "2rem"}}>
-            <Typography variant="h3" gutterBottom>
-                {translation('portfolio_title')}
-            </Typography>
-            <Typography variant="body1" gutterBottom align="justify">
-                {translation('portfolio_body')}
-            </Typography>
-            <DataTable data={portfolioData} headers={headers}/>
-        </CardContent>
+        <Typography variant="h3" gutterBottom>
+            {translation('portfolio_title')}
+        </Typography>
+        <Typography variant="body1" gutterBottom align="justify">
+            {translation('portfolio_body')}
+        </Typography>
+        <DataTable data={portfolioData} headers={headers}/>
     </Paper>);
 };
 export default Portfolio;
